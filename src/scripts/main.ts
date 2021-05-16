db71349cf594328f09cadbe06d7555db60357209
Nils <nils.yseboodt@gmail.com>
2021-05-16 03:43:58 +0200

    show information in a popup

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-import { staticValues } from "./static-values";
+import { staticValues } from "./static-values.js";
 
 interface CombatData {
   current: {round: number, turn: number, tokenId: string},
@@ -13,27 +13,77 @@ interface CombatData {
   }
 }
 
-const onUpdateCombat = (combat: CombatData, update: Combat, options: any) => {
+let openDialogs: Dialog[] = [];
+const reminderContentClass = `${staticValues.moduleName}-reminder-content`;
+function setPopupContent(): void {
+  const templateData = {
+    reminders: [
+      'Knowledge check',
+      'Movement',
+      'Communicate',
+      '1 Object interaction',
+      '1 Action',
+      '1 Bonus Action',
+    ]
+  };
+
+  renderTemplate(`modules/${staticValues.moduleName}/templates/reminder.hbs`, templateData).then((content: any/* string */) => {
+    const popups = document.querySelectorAll(`.${reminderContentClass} .dialog-content`);
+    if (popups.length === 0) {
+      const dialog = new Dialog({
+        title: 'Turn reminder',
+        content: content,
+        buttons: {},
+        close: () => {
+          const remainingDialogs: Dialog[] = [];
+          for (const openDialog of openDialogs) {
+            if (dialog !== openDialog) {
+              remainingDialogs.push(dialog);
+            }
+          }
+          openDialogs = remainingDialogs;
+        }
+      }, {
+        classes: [reminderContentClass]
+      });
+      dialog.render(true);
+      openDialogs.push(dialog);
+    } else {
+      for (const dialog of openDialogs) {
+        if (dialog.element instanceof HTMLElement) {
+          dialog.element.querySelector('.dialog-content').innerHTML = content;
+        } else {
+          dialog.element.find('.dialog-content').html(content);
+        }
+        dialog.maximize();
+      }
+    }
+  });
+}
+
+Hooks.on("ready", (combat: CombatData, update: Combat, options: any) => {
+  if (game.combat) {
+    setPopupContent();
+  }
+
+  return true;
+});
+
+Hooks.on("updateCombat", (combat: CombatData, update: Combat, options: any) => {
   if (update.round !== undefined || update.turn !== undefined) {
     const turn = combat.turns[combat.current.turn];
     if (turn.players.length > 0) {
-      const templateData = {
-        reminders: ['Knowledge check']
-      };
-      
-      renderTemplate(`modules/${staticValues.moduleName}/templates/reminder.hbs`, templateData).then(content => {
-        ChatMessage.create({
-          speaker: ChatMessage.getSpeaker({alias: staticValues.moduleLabel}),
-          content: content,
-          type: CONST.CHAT_MESSAGE_TYPES.OTHER,
-          user: game.userId
-        });
-      });
-
+      setPopupContent();
     }
   }
 
   return true;
-}
+});
 
-Hooks.on("updateCombat", onUpdateCombat);
+Hooks.on("deleteCombat", (combat: CombatData, update: Combat, options: any) => {
+  for (const dialog of openDialogs) {
+    dialog.close();
+  }
+
+  return true;
+});
